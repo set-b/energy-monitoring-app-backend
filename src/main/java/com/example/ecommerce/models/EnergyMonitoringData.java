99206afa9aed20c7d859b2ff9a29a8b967a21f53
brandyn <brandyntse941@gmail.com>
2026-09-02 12:38:53 +0200

    created energy monitoring data entity; embeddable measurement entity has not been created yet

--- src/main/java/com/example/ecommerce/models/EnergyMonitoringData.java
+++ src/main/java/com/example/ecommerce/models/EnergyMonitoringData.java
@@ -2,11 +2,33 @@ package com.example.ecommerce.models;
 
 import jakarta.persistence.*;
 
+import java.time.Instant;
+
+// TODO delete CSV content column!!
 @Entity
 public class EnergyMonitoringData {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // timestamp
+    @Column(nullable = false)
+    private Instant timestamp;
+
+    // TODO combine _value and _field into an Embeddable
+    // private Measurement measurement
+
+    // CTYPE
+    @Column(nullable = false)
+    private String commodityType;
+
+    // CTYPEC
+    @Column(nullable = true)
+    private String commodityCategory;
+
+    // ETYPE - priceprovider is the only value that is NOT a DEVICE
+    @Column(nullable = false)
+    private String deviceType;
 
     // Lob = "large object"
     @Lob
@@ -15,12 +37,56 @@ public class EnergyMonitoringData {
 
     // getters/setters
 
+    public EnergyMonitoringData() {
+    }
+
+    public EnergyMonitoringData(Long id, Instant timestamp, String commodityType, String commodityCategory, String deviceType, String content) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.commodityType = commodityType;
+        this.commodityCategory = commodityCategory;
+        this.deviceType = deviceType;
+        this.content = content;
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getCommodityType() {
+        return commodityType;
+    }
+
+    public void setCommodityType(String commodityType) {
+        this.commodityType = commodityType;
+    }
+
+    public String getCommodityCategory() {
+        return commodityCategory;
+    }
+
+    public void setCommodityCategory(String commodityCategory) {
+        this.commodityCategory = commodityCategory;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
     }
 
     public String getContent() {
